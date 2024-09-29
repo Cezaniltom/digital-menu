5c0f0f5e5e7e352c0bc5d6b10c0d9e0c30569008
Evangelista Nobre <38170234+evannobre@users.noreply.github.com>
2024-09-29 17:21:51 -0300

    Changing input type
    Change applied for product name

--- Projeto-final/components/form.component.ts
+++ Projeto-final/components/form.component.ts
@@ -13,27 +13,17 @@ export default class FormComponent {
             </div> 
             <div class=" input-group mb-3">
                 <label class="input-group-text" for="nome">Nome do Produto</label>
-                <select class="form-control" id="nome" name="nome">
-                    <option value="X-BURGER">X-BURGER</option>
-                    <option value="X-FOME">X-FOME</option>
-                    <option value="X-VEGAN">X-VEGAN</option>
-                    <option value="X-MINI">X-MINI</option>
-                    <option value="X-TUDO">"X-TUDO"</option>
-                    <option value="X-MINI EM DOBRO">X-MINI EM DOBRO</option>
-                    <option value="X-TRIPLO">X-TRIPLO</option>
-                    <option value="X-TUDO EM DOBRO">X-TUDO EM DOBRO</option>
-                    <option value="X-MODA DA CASA">X-MODA DA CASA</option>
-                </select>
+                <input type="text" class="form-control" id="nome" name="nome" placeholder="Ex: X-tudo" required/>
             </div>
             <div class="input-group mb-3">
                 <label class="input-group-text" for="preco">Valor</label>
-                <input type="number" class="form-control" id="preco" name="preco" placeholder="Ex: 20" required>
+                <input type="number" class="form-control" id="preco" name="preco" placeholder="Ex: 20" required/>
             </div>   
             <div class="input-group">
                 <label class="input-group-text" for="ingredientes">Ingredientes</label>
                 <textarea class="form-control" id="ingredientes" name="ingredientes" required></textarea>
             </div>
-            <button type="submit" class="btn btn-outline-light mt-2">Cadastrar</button>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
         `;
     }
 }
--- Projeto-final/models/burger.ts
+++ Projeto-final/models/burger.ts
@@ -1,9 +1,8 @@
 export type Ingredientes = "Pão brioche" | "Pão australiano" | "Alface" | "Tomate" | "Cebola" | "Rúcula" | "Cebola caramelizada" | "Picles" | "Picles de cebola roxa" | "Bacon" | "Farofa de bacon" | "Queijo muçarela" | "Queijo cheddar" | "Blend artesanal de carne" | "Blend artesanal de porco" | "Carne do sol" | "Maionese da casa" | "Molho ranch" | "Barbecue" | "Mostarda de dijon";
-export type Nomes = "X-BURGER" | "X-FOME" | "X-VEGAN" | "X-MINI" | "X-TUDO" | "X-MINI EM DOBRO" | "X-TRIPLO" | "X-TUDO EM DOBRO" | "X-MODA DA CASA";
 
 export default interface Burger {
     fotoProduto: string,
-    nome: Nomes[],
+    nome: string,
     preco: number,
     descricao: Ingredientes[],
 }
